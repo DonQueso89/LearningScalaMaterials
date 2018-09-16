c459a978394ec1fbe1de1b7ff09e545e2408fb02
Kees van Ekeren <kees.van.ekeren@mobpro.com>
2018-09-16 14:40:43 +0200

    Chap8 exercises

--- Solutions/src/exercises/ch8/ex2.scala
+++ Solutions/src/exercises/ch8/ex2.scala
@@ -26,17 +26,34 @@ class Node[T](elements: T*) {
 abstract class BaseNode[T] {
   def foreach(f: T => Unit): Unit
   def apply(index: Int): Option[T]
+  def size: Int
   def headOption: Option[T] = apply(0)
+  def _filter(f: T => Boolean): Seq[T]
+  def _map[X](f: T => X): Seq[X]
+  def head: T = {
+    val value = headOption
+    if (value.isEmpty) {
+      throw new java.util.NoSuchElementException
+    }
+    value.get
+  }
+  def tail: BaseNode[T]
 }
 
 class NullNode[T] extends BaseNode[T] {
   override def foreach(f: T => Unit): Unit = {}
   override def apply(index: Int): Option[T] = None
+  override def _filter(f: T => Boolean): Seq[T] = Seq[T]()
+  override def _map[X](f: T => X): Seq[X] = Seq[X]()
+  override def tail: BaseNode[T] = throw new java.lang.UnsupportedOperationException
+  def size: Int = 0
+  override def toString: String = s""
 }
 
 class ListNode[T](elements: T*) extends BaseNode[T] {
   val value: Option[T] = elements.headOption
-  val nextNode = if (elements.tail.isEmpty) new NullNode else new ListNode[T](elements.tail:_*)
+  val nextNode: BaseNode[T] = if (elements.tail.isEmpty) new NullNode else new ListNode[T](elements.tail:_*)
+
   def apply(index: Int): Option[T] = {
     if (index < 1) {
       value
@@ -50,5 +67,44 @@ class ListNode[T](elements: T*) extends BaseNode[T] {
       case node: ListNode[T] => f(value.get); nextNode.foreach(f);
       case _ => f(value.get);
     }
+  }
+
+  override def tail(): BaseNode[T] = {
+    nextNode
+  }
+  
+  def size: Int = { 
+    1 + nextNode.size
+  }
+
+  override def _filter(f: T => Boolean): Seq[T] = {
+    /*
+     * Ugly helper func for filter method to construct filtered Sequence
+     */
+    ( if (f(value.get)) Seq[T](value.get) else Seq[T]() ) ++ nextNode._filter(f)
+  }
+
+  def filter(f: T => Boolean): ListNode[T] = {
+    val result: Seq[T] = ( if (f(value.get)) Seq[T](value.get) else Seq[T]() ) ++ nextNode._filter(f)
+    new ListNode[T](result:_*)
+  }
+
+  override def _map[X](f: T => X): Seq[X] = {
+    /*
+     * Ugly helper function for map method to construct mapped Sequence
+     */
+    Seq[X](f(value.get)) ++ nextNode._map(f)
+  }
+  
+
+  def map[X](f: T => X): ListNode[X] = {
+    /*
+     * Construct mapped Sequence from Sequence obtained from helper meth
+     */
+    new ListNode[X](_map(f):_*)
+  }
+
+  override def toString: String = {
+    s"${value.get} " + nextNode.toString
   }
 }
